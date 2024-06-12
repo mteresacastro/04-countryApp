8fc6aa740ee6e4975474f029f4979952d43938b6
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-06-12 17:41:52 +0200

    add country interface

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,12 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
-  templateUrl: './search-box.component.html',
+  templateUrl: 'search-box.component.html',
   styles: ``
 })
 export class SearchBoxComponent {
 
   @Input()
   public placeholder: string = '';
+
+  @Output()
+  public onValue = new EventEmitter<string>();
+
+  emitValue(value: string): void {
+    this.onValue.emit(value);
+  }
 }
