f5466b95d9d98a9bdaff9b4677dd0d7965a22edc
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-07-02 12:04:16 +0200

    unsubscribe

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,14 +1,16 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: 'search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
+
 
   private debouncer: Subject<string> = new Subject<string>(); //Subject es un observable de rxjs
+  private debouncerSubscription?: Subscription;
 
   @Input()
   public placeholder: string = '';
@@ -20,13 +22,18 @@ export class SearchBoxComponent implements OnInit {
   public onDebounce = new EventEmitter<string>();
 
   ngOnInit(): void {
-      this.debouncer
+      this.debouncerSubscription = this.debouncer
       .pipe(
         debounceTime(500) // Esto hace que continue solo si pasa 0.5 seg
       )
       .subscribe( value =>{
         this.onDebounce.emit(value);
       })
+  }
+
+  ngOnDestroy(): void {
+    //Destruye el componente cuando sale de el.
+    this.debouncerSubscription?.unsubscribe();
   }
 
   emitValue(value: string): void {
